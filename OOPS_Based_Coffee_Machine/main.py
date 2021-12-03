c51eebd00a5cce893b6a729cb30293fd749a4b98
Mohd Wajid <88840217+mohdWajid19@users.noreply.github.com>
2021-12-03 20:12:54 +0530

    Update main.py

--- OOPS_Based_Coffee_Machine/main.py
+++ OOPS_Based_Coffee_Machine/main.py
@@ -13,10 +13,8 @@ while is_true:
     clear()
     user_need = input(f"WHAT Would you like to have,\n{menu.get_items()} enter the name of coffee : ")
     if user_need == 'espresso' or user_need == "latte" or user_need == 'cappuccino':
-        for item in  menu.menu: #get cost from menu list in Menu class
-            if item.name == user_need:
-                cost = item.cost #storing cost of item
         user_wants = menu.find_drink(user_need) #store the object of drink user needs
+        cost = user_wants.cost #cost of the drink from user wants object
         if coffee_maker.is_resource_sufficient(user_wants): #check if sufficient resources are available
             if money_machine.make_payment(cost): #check if payment is done or not
                 coffee_maker.make_coffee(user_wants) #make coffee
